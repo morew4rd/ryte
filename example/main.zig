fa6970aa836f5c2d528c08a8b9d3d7372b5896c5
mg <morew4rd@gmail.com>
2024-12-28 00:51:07 -0800

    glfw example: open window

--- example/main.zig
+++ example/main.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 const glfw = @import("glfw");
 
+const W = 800;
+const H = 600;
+
 pub fn main() !void {
     std.debug.print("ryte example.\n", .{});
     if (glfw.glfwInit() == 0) {
@@ -8,5 +11,14 @@ pub fn main() !void {
         _ = glfw.glfwGetError(&err);
         std.debug.print("glfw init failed: {s}\n", .{err});
         return error.GlfwInitFailed;
+    }
+    const win = glfw.glfwCreateWindow(W, H, "hello", null, null);
+    if (win == null) {
+        return error.CouldntOpenWindow;
+    }
+    defer glfw.glfwTerminate();
+
+    while (glfw.glfwWindowShouldClose(win) == 0) {
+        glfw.glfwPollEvents();
     }
 }
