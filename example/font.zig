ad87a348d5bb4c725682232a9e15aa3990d603f4
mg <morew4rd@gmail.com>
2025-01-04 08:09:30 -0800

    font: works in windows/mac TBD for wasm (filesystem is missing there)

--- example/font.zig
+++ example/font.zig
@@ -1,2 +1,295 @@
 const std = @import("std");
+const sg = @import("sokol_gfx");
+const sgp = @import("sokol_gp");
 const fnts = @import("fontstash");
+
+const window = @import("window.zig");
+
+const K_FONT_ATLAS_SIZE = 1024;
+
+pub const KyteFont = struct {
+    fonsctx: *fnts.FONScontext, // align(16),
+    fontdata: []u8, // align(16),
+    buffer: []u8, // align(16),
+    _image_handle: sg.sg_image,
+    _sampler_handle: sg.sg_sampler,
+    width: c_int,
+    height: c_int,
+    font: c_int,
+    atlas_dim: c_int,
+    fontsize: f32,
+    allocator: std.mem.Allocator,
+};
+
+pub fn loadFontFromFile(allocator: std.mem.Allocator, fullpath: []const u8, initheight: f32) !*KyteFont {
+    const file = try std.fs.cwd().openFile(fullpath, .{});
+    defer file.close();
+
+    const fontdata = try file.readToEndAlloc(allocator, std.math.maxInt(usize));
+    errdefer allocator.free(fontdata);
+
+    return try loadFontFromMemory(allocator, fontdata, fullpath, initheight);
+}
+
+pub fn loadFontFromMemory(allocator: std.mem.Allocator, fontdata: []u8, name: []const u8, initheight: f32) !*KyteFont {
+    const font = try allocator.create(KyteFont);
+    errdefer allocator.destroy(font);
+
+    font.* = .{
+        .fonsctx = undefined,
+        .fontdata = fontdata,
+        .buffer = undefined,
+        ._image_handle = undefined,
+        ._sampler_handle = undefined,
+        .width = 0,
+        .height = 0,
+        .font = 0,
+        .atlas_dim = K_FONT_ATLAS_SIZE,
+        .fontsize = initheight,
+        .allocator = allocator,
+    };
+
+    var params = fnts.FONSparams{
+        .flags = fnts.FONS_ZERO_TOPLEFT,
+        .width = font.atlas_dim,
+        .height = font.atlas_dim,
+        .userPtr = font,
+        .renderCreate = fonsRenderCreate,
+        .renderDelete = fonsRenderDelete,
+        .renderResize = fonsRenderResize,
+        .renderUpdate = fonsRenderUpdate,
+        .renderDraw = fonsRenderDraw,
+    };
+
+    font.fonsctx = fnts.fonsCreateInternal(@ptrCast(&params)) orelse return error.FontCreationFailed;
+    font.font = fnts.fonsAddFontMem(font.fonsctx, name.ptr, fontdata.ptr, @intCast(fontdata.len), 0);
+
+    if (font.font == fnts.FONS_INVALID) {
+        return error.FontLoadFailed;
+    }
+
+    return font;
+}
+
+pub fn destroyFont(allocator: std.mem.Allocator, font: *KyteFont) void {
+    fnts.fonsDeleteInternal(font.fonsctx);
+    allocator.free(font.fontdata);
+    allocator.destroy(font);
+}
+
+pub fn setFontHeight(font: *KyteFont, newheight: f32) void {
+    font.fontsize = newheight;
+}
+
+pub fn getFontHeight(font: *KyteFont) f32 {
+    return font.fontsize;
+}
+
+// fontstash callbacks
+fn fonsRenderCreate(user_ptr: ?*anyopaque, width: c_int, height: c_int) callconv(.c) c_int {
+    const kfont: *KyteFont = @ptrCast(@alignCast(user_ptr));
+    kfont.width = width;
+    kfont.height = height;
+
+    const imdesc = sg.sg_image_desc{
+        .width = width,
+        .height = height,
+        .pixel_format = sg.SG_PIXELFORMAT_RGBA8,
+        .type = sg.SG_IMAGETYPE_2D,
+        .usage = sg.SG_USAGE_DYNAMIC,
+    };
+    const img = sg.sg_make_image(&imdesc);
+
+    const smpdesc = sg.sg_sampler_desc{
+        .min_filter = @intFromEnum(window.main_window.filtermode),
+        .mag_filter = @intFromEnum(window.main_window.filtermode),
+    };
+    const smp = sg.sg_make_sampler(&smpdesc);
+
+    kfont._image_handle = img;
+    kfont._sampler_handle = smp;
+
+    // Use the same allocator that was used to create the font
+    kfont.buffer = kfont.allocator.alloc(u8, @intCast(width * height * 4)) catch &[_]u8{};
+    @memset(kfont.buffer, 0); // Initialize with zeros
+
+    return 1;
+}
+
+fn fonsRenderDelete(user_ptr: ?*anyopaque) callconv(.c) void {
+    const kfont: *KyteFont = @ptrCast(@alignCast(user_ptr));
+    sg.sg_destroy_image(kfont._image_handle);
+    sg.sg_destroy_sampler(kfont._sampler_handle);
+
+    // Use the allocator stored in the font object
+    if (kfont.buffer.len > 0) {
+        kfont.allocator.free(kfont.buffer);
+    }
+}
+
+fn fonsRenderResize(user_ptr: ?*anyopaque, width: c_int, height: c_int) callconv(.c) c_int {
+    fonsRenderDelete(user_ptr);
+    return fonsRenderCreate(user_ptr, width, height);
+}
+
+fn fonsRenderUpdate(user_ptr: ?*anyopaque, rect: [*c]c_int, data: [*c]const u8) callconv(.c) void {
+    const kfont: *KyteFont = @ptrCast(@alignCast(user_ptr));
+    const width = kfont.width;
+    // const height = kfont.height;
+
+    const X1 = rect[0];
+    const Y1 = rect[1];
+    const X2 = rect[2];
+    const Y2 = rect[3];
+
+    var mydata = kfont.buffer;
+
+    var hh: c_int = 0;
+    var ww: c_int = 0;
+    var iy = Y1;
+    while (iy < Y2) : (iy += 1) {
+        hh += 1;
+        ww = 0;
+        var ix = X1;
+        while (ix < X2) : (ix += 1) {
+            ww += 1;
+            const i: usize = @intCast(ix + iy * width);
+            const val = data[i];
+            mydata[i * 4] = 255;
+            mydata[i * 4 + 1] = 255;
+            mydata[i * 4 + 2] = 255;
+            mydata[i * 4 + 3] = val;
+        }
+    }
+
+    // Create proper image data structure
+    var imdat = sg.sg_image_data{};
+    imdat.subimage[0][0] = .{
+        .ptr = mydata.ptr,
+        .size = @intCast(mydata.len),
+    };
+
+    sg.sg_update_image(kfont._image_handle, &imdat);
+}
+
+fn fonsRenderDraw(user_ptr: ?*anyopaque, verts: [*c]const f32, tcoords: [*c]const f32, colors: [*c]const u32, nverts: c_int) callconv(.c) void {
+    _ = colors;
+    const kfont: *KyteFont = @ptrCast(@alignCast(user_ptr));
+    const width: f32 = @floatFromInt(kfont.width);
+    const height: f32 = @floatFromInt(kfont.height);
+
+    sgp.sgp_set_image(0, @bitCast(kfont._image_handle));
+    sgp.sgp_set_sampler(0, @bitCast(kfont._sampler_handle));
+
+    var i: usize = 0;
+    while (i < nverts - 2) : (i += 3) {
+        var q: usize = 0;
+        const p1x = verts[(q + i) * 2];
+        const p1y = verts[(q + i) * 2 + 1];
+        const t1x = tcoords[(q + i) * 2];
+        const t1y = tcoords[(q + i) * 2 + 1];
+        q += 1;
+        // const p2x = verts[(q + i) * 2];
+        // const p2y = verts[(q + i) * 2 + 1];
+        // const t2x = tcoords[(q + i) * 2];
+        // const t2y = tcoords[(q + i) * 2 + 1];
+        q += 1;
+        const p3x = verts[(q + i) * 2];
+        const p3y = verts[(q + i) * 2 + 1];
+        const t3x = tcoords[(q + i) * 2];
+        const t3y = tcoords[(q + i) * 2 + 1];
+
+        sgp.sgp_push_transform();
+        sgp.sgp_translate(p1x, p1y);
+        sgp.sgp_draw_textured_rect(0, .{
+            .x = 0,
+            .y = 0,
+            .w = p3x - p1x,
+            .h = p3y - p1y,
+        }, .{
+            .x = t1x * width,
+            .y = t1y * height,
+            .w = (t3x - t1x) * width,
+            .h = (t3y - t1y) * height,
+        });
+        sgp.sgp_pop_transform();
+    }
+
+    sgp.sgp_reset_image(0);
+}
+
+pub const FontContext = struct {
+    current_font: ?*KyteFont = null,
+    // Add other necessary state here
+};
+
+var ctx: FontContext = .{};
+
+pub fn setCurrentFont(font: *KyteFont) void {
+    ctx.current_font = font;
+}
+
+pub fn getCurrentFont() ?*KyteFont {
+    return ctx.current_font;
+}
+
+pub fn drawText(text: []const u8, x: f32, y: f32) !void {
+    const current_font = ctx.current_font orelse return error.NoFontSet;
+
+    if (current_font.font == fnts.FONS_INVALID) {
+        return error.InvalidFont;
+    }
+
+    const ctx_fons = current_font.fonsctx;
+
+    fnts.fonsClearState(ctx_fons);
+    fnts.fonsSetFont(ctx_fons, current_font.font);
+    fnts.fonsSetSize(ctx_fons, current_font.fontsize);
+
+    var fontheight: f32 = 0;
+    fnts.fonsVertMetrics(ctx_fons, null, null, &fontheight);
+    fnts.fonsSetBlur(ctx_fons, 0);
+
+    const last_x = fnts.fonsDrawText(ctx_fons, x, y + fontheight, text.ptr, null);
+
+    // You might want to return the last_x position if needed
+    _ = last_x;
+}
+
+pub fn getTextSize(text: []const u8) !struct { width: f32, height: f32 } {
+    const current_font = ctx.current_font orelse return error.NoFontSet;
+
+    if (current_font.font == fnts.FONS_INVALID) {
+        return error.InvalidFont;
+    }
+
+    const ctx_fons = current_font.fonsctx;
+
+    var bounds: [4]f32 = undefined;
+    fnts.fonsTextBounds(ctx_fons, 0, 0, text.ptr, null, &bounds);
+
+    return .{
+        .width = bounds[2] - bounds[0],
+        .height = bounds[3] - bounds[1],
+    };
+}
+
+pub fn drawTextWithSize(text: []const u8, x: f32, y: f32) !struct { width: f32, height: f32 } {
+    const current_font = ctx.current_font orelse return error.NoFontSet;
+
+    if (current_font.font == fnts.FONS_INVALID) {
+        return error.InvalidFont;
+    }
+
+    const ctx_fons = current_font.fonsctx;
+
+    var bounds: [4]f32 = undefined;
+    fnts.fonsTextBounds(ctx_fons, x, y, text.ptr, null, &bounds);
+
+    try drawText(text, x, y);
+
+    return .{
+        .width = bounds[2] - bounds[0],
+        .height = bounds[3] - bounds[1],
+    };
+}
--- example/main.zig
+++ example/main.zig
@@ -8,10 +8,22 @@ const sgp = @import("sokol_gp");
 const window = @import("window.zig");
 const input = @import("input.zig");
 const canvas = @import("canvas.zig");
+const font = @import("font.zig");
 
 var angle: f32 = 0.3;
 
 var cvs: *canvas.KyteImage = undefined;
+var font1: *font.KyteFont = undefined;
+var font2: *font.KyteFont = undefined;
+
+fn print_path() !void {
+    if (builtin.os.tag != .emscripten) {
+        // print current working directory.
+        var cwd_buf: [std.fs.max_path_bytes]u8 = undefined;
+        const cwd = try std.fs.cwd().realpath(".", &cwd_buf);
+        std.debug.print("CWD: {s}\n", .{cwd});
+    }
+}
 
 pub fn main() !void {
     std.debug.print("ryte example.\n", .{});
@@ -32,6 +44,11 @@ pub fn main() !void {
     try input.initInputCallbacks(window.main_window.window);
 
     cvs = try canvas.newCanvas(200, 200);
+    font1 = try font.loadFontFromFile(std.heap.c_allocator, "assets/m5x7.ttf", 14); // TODO: store font on the object
+    font2 = try font.loadFontFromFile(std.heap.c_allocator, "assets/DroidSansMono.ttf", 32); // TODO: store font on the object
+    defer font.destroyFont(std.heap.c_allocator, font1); // crash
+    defer font.destroyFont(std.heap.c_allocator, font2); // crash
+    font.setCurrentFont(font1);
 
     // TODO where does this go?
     sgp.sgp_set_blend_mode(sgp.SGP_BLENDMODE_BLEND);
@@ -59,11 +76,18 @@ fn tickFn(ts: window.TickState) void {
 
     sgp.sgp_rotate(angle);
 
-    sgp.sgp_set_color(1, 1, 0, 1);
+    sgp.sgp_set_color(1, 1, 0, 0.5);
     sgp.sgp_draw_filled_rect(20, 20, 300, 300);
 
     sgp.sgp_reset_transform();
     sgp.sgp_reset_color();
     sgp.sgp_set_image(0, @bitCast(cvs._image_handle));
     sgp.sgp_draw_textured_rect(0, .{ .x = 0, .y = 0, .h = 200, .w = 200 }, .{ .x = 0, .y = 0, .h = 200, .w = 200 });
+
+    // sgp.sgp_scale(2, 2);
+    sgp.sgp_set_color(1, 1, 1, 1);
+    // font.setCurrentFont(font1);
+    // font.drawText("lyte2d in zig", 10, 10) catch {};
+    font.setCurrentFont(font2);
+    font.drawText("lyte2d in zig", 10, 50) catch {};
 }
